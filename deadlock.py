af3271263eb661e4b845740ecf048272e2c40e7b
Michael Whittaker <mjwhittaker@berkeley.edu>
2018-12-21 22:22:15 -0800

    Deadlock fixes.

--- deadlock.py
+++ deadlock.py
@@ -1,83 +1,140 @@
 from collections import defaultdict
-from itertools import islice, chain, combinations, combinations_with_replacement, permutations, product
 from multiprocessing import cpu_count, Pool, Process, Queue
+from typing import DefaultDict, FrozenSet, Iterable, Iterator, Tuple, TypeVar
+import itertools
 import math
 import operator as op
 import os
 
-class Ordering(object):
-    def __init__(self, n, commands=None):
-        self.n = n
-        self.commands = commands or tuple(range(n))
-        self.commands = tuple(self.commands)
+Command = int
+Ordering = Tuple[Command, ...]
+GlobalOrdering = Tuple[Ordering, ...]
 
-    def __str__(self):
-        return str(self.commands)
 
-    def __repr__(self):
-        return str(self)
+T = TypeVar('T')
+def batched(iterable: Iterable[T], size: int) -> Iterator[Tuple[T, ...]]:
+    """https://docs.python.org/3/library/itertools.html#recipes"""
+    args = [iter(iterable)] * size
+    return itertools.zip_longest(*args, fillvalue=None)
 
-    def deps(self, command):
-        conflicts = {self.next_conflict(command), self.prev_conflict(command)}
-        prefix = self.commands[:self.commands.index(command)]
-        return frozenset([c for c in prefix if c in conflicts])
 
-    def next_conflict(self, cmd):
-        return (cmd + 1) % self.n
+def prod(xs: Iterable[int]) -> int:
+    p = 1
+    for x in xs:
+        p *= x
+    return p
 
-    def prev_conflict(self, cmd):
-        return (cmd - 1) % self.n
 
-    @staticmethod
-    def permutations(n):
-        for permutation in permutations(range(n)):
-            yield Ordering(n, permutation)
-
-def majority_size(n):
-    return int(math.floor(float(n) / 2) + 1)
-
-def exists_majority_dep(global_ordering, command):
-    next_conflict = global_ordering[0].next_conflict(command)
-    deps_count = defaultdict(int)
-    for ordering in global_ordering:
-        deps_count[ordering.deps(command)] += 1
-    for (deps, count) in deps_count.items():
-        if (count >= majority_size(len(global_ordering)) and
-            next_conflict not in deps and
-            any(next_conflict in d for (d, c) in deps_count.items())):
-            return True
-    return False
-
-def is_global_ordering_deadlocked(global_ordering):
-    return all(exists_majority_dep(global_ordering, command)
-               for command in global_ordering[0].commands)
-
-def ncr(n, r):
+def choose(n: int, r: int) -> int:
     """https://stackoverflow.com/a/4941932/3187068"""
     r = min(r, n-r)
-    numer = reduce(op.mul, range(n, n-r, -1), 1)
-    denom = reduce(op.mul, range(1, r+1), 1)
-    return numer / denom
+    numer = prod(range(n, n-r, -1))
+    denom = prod(range(1, r+1))
+    return int(numer / denom)
 
-def deadlock_possible(queue, num_replicas, num_commands):
-    print("num_replicas={} num_commands={}".format(num_replicas, num_commands))
-    global_orderings = combinations_with_replacement(
-        Ordering.permutations(num_commands), num_replicas)
 
-    i = 0
-    total = ncr(num_replicas + math.factorial(num_commands) - 1, num_replicas)
-    batch_size = 10000
-    for global_ordering_batch in batch(global_orderings, batch_size):
-        queue.put(global_ordering_batch)
-        i += batch_size
-        if (i % 100000) == 0:
-            print("{}/{}".format(i, total))
+def majority_size(n: int) -> int:
+    return int(math.floor(n / 2) + 1)
 
-def batch(iterable, size):
-    sourceiter = iter(iterable)
-    while True:
-        batchiter = islice(sourceiter, size)
-        yield chain([batchiter.next()], batchiter)
+
+class Ord(object):
+    @staticmethod
+    def all(n: int) -> Iterator[Ordering]:
+        for permutation in itertools.permutations(range(n)):
+            yield permutation
+
+    @staticmethod
+    def num(num_commands: int) -> int:
+        return math.factorial(num_commands)
+
+    @staticmethod
+    def next_conflict(n: int, cmd: Command):
+        return (cmd + 1) % n
+
+    @staticmethod
+    def prev_conflict(n: int, cmd: Command):
+        return (cmd - 1) % n
+
+    @staticmethod
+    def deps(ordering: Ordering, cmd: Command) -> FrozenSet[Command]:
+        n = len(ordering)
+        conflicts = {
+            Ord.next_conflict(n, cmd),
+            Ord.prev_conflict(n, cmd),
+        }
+        prefix = ordering[:ordering.index(cmd)]
+        return frozenset([c for c in prefix if c in conflicts])
+
+class GlobalOrd(object):
+    @staticmethod
+    def all(num_replicas: int, num_commands: int) -> Iterable[GlobalOrdering]:
+        combos = itertools.combinations_with_replacement
+        return combos(Ord.all(num_commands), num_replicas)
+
+    @staticmethod
+    def num(num_replicas: int, num_commands: int) -> int:
+        n = Ord.num(num_commands)
+        r = num_replicas
+        return choose(n - 1 + r, r)
+
+    @staticmethod
+    def exists_deferred_majority(
+        global_ordering: GlobalOrdering,
+        cmd: Command
+    ) -> bool:
+        """Returns whether there is an equal majority with dissenting dep.
+
+        Given a global ordering and command, `exists_deferred_majority` returns
+        whether there exists a majority of nodes that agree on the deps of
+        `cmd` and exclude `cmd + 1` from these deps and exists some node with
+        deps that does include `cmd + 1`. This is the sitaution in which BPaxos
+        defers recovery of `cmd`.
+        """
+        num_replicas = len(global_ordering)
+        num_commands = len(global_ordering[0])
+
+        # Compute a histogram of the nodes' deps.
+        deps_count: DefaultDict[FrozenSet[Command], int] = defaultdict(int)
+        for ordering in global_ordering:
+            deps_count[Ord.deps(ordering, cmd)] += 1
+
+        # Check the conditions listed in the docstring.
+        next_conflict = Ord.next_conflict(num_commands, cmd)
+        return (any(next_conflict in deps for deps in deps_count) and
+                any(count >= majority_size(num_replicas) and
+                    next_conflict not in deps
+                    for (deps, count) in deps_count.items()))
+
+    @staticmethod
+    def is_deadlocked(global_ordering: GlobalOrdering) -> bool:
+        return all(GlobalOrd.exists_deferred_majority(global_ordering, cmd)
+                   for cmd in global_ordering[0])
+
+def is_deadlock_possible(
+    queue: Queue,
+    num_replicas: int,
+    num_commands: int
+) -> None:
+    global_orderings = GlobalOrd.all(num_replicas, num_commands)
+    num = GlobalOrd.num(num_replicas, num_commands)
+    batch_size = int(100 * 1000)
+
+    if queue:
+        i = 0
+        for batch in batched(global_orderings, batch_size):
+            queue.put(batch)
+            i += batch_size
+            print("{}/{}".format(i, num))
+    else:
+        i = 0
+        for global_ordering in global_orderings:
+            if GlobalOrd.is_deadlocked(global_ordering):
+                print(global_ordering)
+
+            i += 1
+            if (i % batch_size) == 0:
+                print("{}/{}".format(i, num))
+
 
 class Worker(Process):
     def __init__(self, queue):
@@ -85,27 +142,30 @@ class Worker(Process):
         super(Worker, self).__init__()
 
     def run(self):
-        global_ordering_batch = self.queue.get()
-        while global_ordering_batch:
-            for global_ordering in global_ordering_batch:
-                if is_global_ordering_deadlocked(global_ordering):
+        batch = self.queue.get()
+        while batch:
+            for global_ordering in batch:
+                if global_ordering and GlobalOrd.is_deadlocked(global_ordering):
                     print(global_ordering)
-            global_ordering_batch = self.queue.get()
+            batch = self.queue.get()
+
 
 def main():
     # Start the workers.
-    queue = Queue(maxsize=cpu_count())
-    workers = [Worker(queue) for _ in range(cpu_count())]
+    num_workers = cpu_count()
+    queue = Queue(maxsize=num_workers)
+    workers = [Worker(queue) for _ in range(num_workers)]
     for worker in workers:
         worker.start()
 
     # Dish out work to the workers.
     for num_commands in range(5, 6):
-        for num_replicas in range(6, 7):
-            deadlock_possible(queue, num_replicas, num_commands)
+        for num_replicas in range(5, 6):
+            # is_deadlock_possible(queue, num_replicas, num_commands)
+            is_deadlock_possible(None, num_replicas, num_commands)
 
     # Wait for the workers.
-    for _ in range(cpu_count()):
+    for _ in range(num_workers):
         queue.put(None)
     for worker in workers:
         worker.join()
